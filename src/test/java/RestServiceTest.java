755ec7d72369f3ea0dd3f16ad44fff552985c42e
AVMihel <dok2000@list.ru>
2025-02-13 19:03:12 +0500

    two

--- src/test/java/RestServiceTest.java
+++ src/test/java/RestServiceTest.java
@@ -1,40 +1,51 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import ru.netology.m3dz5_1.services.RestService;
 
 public class RestServiceTest {
 
-    @Test
-    void calculationOfRestMonths() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/rest.csv")
+    void calculationOfRestMonths(int income, int expenses, int threshold, int expected) {
         RestService service = new RestService();
 
-        // подготавливаем данные:
-        int income = 10000;
-        int expenses = 3000;
-        int threshold = 20000;
-        int expected = 4;
-
-        // вызываем целевой метод:
         int actual = service.calcRest(income, expenses, threshold);
 
-        // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    void calculationOfBigRestMonths() {
-        RestService service = new RestService();
+//    @Test
+//    void calculationOfRestMonths() {
+//        RestService service = new RestService();
+//
+    // подготавливаем данные:
+//        int income = 10000;
+//        int expenses = 3000;
+//        int threshold = 20000;
+//        int expected = 4;
+//
+    // вызываем целевой метод:
+//        int actual = service.calcRest(income, expenses, threshold);
+//
+    // производим проверку (сравниваем ожидаемый и фактический):
+//        Assertions.assertEquals(expected, actual);
+//  }
 
-        // подготавливаем данные:
-        int income = 100000;
-        int expenses = 50000;
-        int threshold = 250000;
-        int expected = 2;
-
-        // вызываем целевой метод:
-        int actual = service.calcRest(income, expenses, threshold);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        Assertions.assertEquals(expected, actual);
-    }
+//    @Test
+//    void calculationOfBigRestMonths() {
+//        RestService service = new RestService();
+    // подготавливаем данные:
+//        int income = 100000;
+//       int expenses = 50000;
+//        int threshold = 250000;
+//        int expected = 2;
+//
+    // вызываем целевой метод:
+//        int actual = service.calcRest(income, expenses, threshold);
+//
+    // производим проверку (сравниваем ожидаемый и фактический):
+//        Assertions.assertEquals(expected, actual);
+//    }
 }
